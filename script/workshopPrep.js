e79d6151e420e5283a38346ceb303d2e383e8e17
Will Weaver <monkey@buildingbananas.com>
2022-06-21 22:16:07 -0400

    Update ci to crate BSON file

--- script/workshopPrep.js
+++ script/workshopPrep.js
@@ -1,12 +1,7 @@
 const BSON = require('bson')
 const fs = require('fs')
 
-console.log("Starting the build process")
-console.log("Reading in the mod file")
 const data = JSON.parse(fs.readFileSync('./mod/2817359776.json'))
-console.log("Converting to BSON")
 BSON.setInternalBufferSize(200000000)
 const serialized = BSON.serialize(data, {})
-console.log("Writing the file out")
 fs.writeFileSync("./mod/MarvelChampions.bson" , serialized)
-console.log("Finished!")
