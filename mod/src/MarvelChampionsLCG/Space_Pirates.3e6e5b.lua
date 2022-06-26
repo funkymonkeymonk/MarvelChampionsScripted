9ef10a7cc303286a1a6b90098e388c69c2bef494
Will Weaver <monkey@buildingbananas.com>
2022-06-26 18:54:01 -0400

    Please work

---
+++ mod/src/MarvelChampionsLCG/Space_Pirates.3e6e5b.lua
@@ -0,0 +1,18 @@
+function onLoad()
+    self.createButton({
+        label="Space Pirates", click_function="buttonPlace", function_owner=self,
+        position={0,4.3,0}, rotation={0,0,0}, height=1000, width=3000,
+        font_size=500, color={0,0,0}, font_color={1,1,1}, tooltip="Add Modular"
+    })
+end
+
+function buttonPlace()
+  self.takeObject({index = 0, position = {-12.75, 1.01, 22.25}, rotation = {0,180,180}})
+end
+
+function onObjectLeaveContainer(container, obj)
+    if container == self then
+        local objClone = obj.clone({position = self.getPosition()})
+        self.putObject(objClone)
+    end
+end
---
+++ mod/src/MarvelChampionsLCG/Wasp/Wasp.States/Wasp.b8f9c3.798900.lua
@@ -0,0 +1,40 @@
+function onload()
+
+   self.createButton({
+      label          = "︿",
+      click_function = "alterEgo",
+      function_owner = self,
+      position       = {0.85,0.3,-0.6},
+      rotation       = {0,0,0},
+      width          = 130,
+      height         = 120,
+      font_size      = 100,
+      color          = {1,0,0},
+      tooltip        = "Alter-Ego"
+   })
+
+   self.createButton({
+      label          = "︽",
+      click_function = "giantForm",
+      function_owner = self,
+      position       = {0.85,0.3,-0.9},
+      rotation       = {0,0,0},
+      width          = 130,
+      height         = 120,
+      font_size      = 100,
+      color          = {1,0,0},
+      tooltip        = "Giant Form"
+   })
+
+end
+
+function giantForm()
+ self.setState(3)
+end
+
+function alterEgo()
+ self.setState(2)
+end
+
+
+
