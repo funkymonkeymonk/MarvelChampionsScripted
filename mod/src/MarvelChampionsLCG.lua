1639877c10c69ccda784d888599a38fab0b9a64c
Will Weaver <monkey@buildingbananas.com>
2022-07-05 16:16:59 -0400

    Fixing lua check errors and updating luacheck script

--- mod/src/MarvelChampionsLCG.lua
+++ mod/src/MarvelChampionsLCG.lua
@@ -381,7 +381,7 @@ function reshuffleEncounterDeck(position, rotation)
          deck.flip()
       end
       deck.shuffle()
-      Wait.time(|| move(deck), 0.3)
+      Wait.time(function() move(deck) end, 0.3)
    else
       printToAll("Couldn't find encounter discard pile to reshuffle", {1,0,0})
    end
