3b428cef4afa10dee536ab6a48ab7fab5b05215a
Will Weaver <monkey@buildingbananas.com>
2022-07-12 16:17:48 -0400

    Build cardpool from cardpool data object

---
+++ mod/src/MarvelChampionsLCG/CardpoolData.lua
@@ -0,0 +1,67 @@
+CARDPOOL_JSON = [[
+[
+  {
+    "code": "12001a",
+    "name": "Ant-Man",
+    "subname": null,
+    "type_code": "hero",
+    "faction_code": "hero",
+    "BackURL": "http://cloud-3.steamusercontent.com/ugc/1795242553066035592/AEE6A404260E9B5DEE79D2B19CB39F982DCA574D/",
+    "FrontURL": "https://cerebrodatastorage.blob.core.windows.net/cerebro-cards/official/12001A.jpg"
+  },
+  {
+    "code": "12001b",
+    "name": "Scott Lang",
+    "subname": null,
+    "type_code": "alter_ego",
+    "faction_code": "hero",
+    "BackURL": "http://cloud-3.steamusercontent.com/ugc/1795242553066035592/AEE6A404260E9B5DEE79D2B19CB39F982DCA574D/",
+    "FrontURL": "https://cerebrodatastorage.blob.core.windows.net/cerebro-cards/official/12001B.jpg"
+  },
+  {
+    "code": "12001c",
+    "name": "Ant-Man",
+    "subname": null,
+    "type_code": "hero",
+    "faction_code": "hero",
+    "BackURL": "http://cloud-3.steamusercontent.com/ugc/1795242553066035592/AEE6A404260E9B5DEE79D2B19CB39F982DCA574D/",
+    "FrontURL": "https://cerebrodatastorage.blob.core.windows.net/cerebro-cards/official/12001C.jpg"
+  },
+  {
+    "code": "12002",
+    "name": "Wasp",
+    "subname": "Nadia Van Dyne",
+    "type_code": "ally",
+    "faction_code": "hero",
+    "BackURL": "http://cloud-3.steamusercontent.com/ugc/1795242553066035592/AEE6A404260E9B5DEE79D2B19CB39F982DCA574D/",
+    "FrontURL": "https://cerebrodatastorage.blob.core.windows.net/cerebro-cards/official/12002.jpg"
+  },
+  {
+    "code": "12003",
+    "name": "Giant Stomp",
+    "subname": null,
+    "type_code": "event",
+    "faction_code": "hero",
+    "BackURL": "http://cloud-3.steamusercontent.com/ugc/1795242553066035592/AEE6A404260E9B5DEE79D2B19CB39F982DCA574D/",
+    "FrontURL": "https://cerebrodatastorage.blob.core.windows.net/cerebro-cards/official/12003.jpg"
+  },
+  {
+    "code": "12004",
+    "name": "Hive Mind",
+    "subname": null,
+    "type_code": "event",
+    "faction_code": "hero",
+    "BackURL": "http://cloud-3.steamusercontent.com/ugc/1795242553066035592/AEE6A404260E9B5DEE79D2B19CB39F982DCA574D/",
+    "FrontURL": "https://cerebrodatastorage.blob.core.windows.net/cerebro-cards/official/12004.jpg"
+  },
+  {
+    "code": "12005",
+    "name": "Resize",
+    "subname": null,
+    "type_code": "event",
+    "faction_code": "hero",
+    "BackURL": "http://cloud-3.steamusercontent.com/ugc/1795242553066035592/AEE6A404260E9B5DEE79D2B19CB39F982DCA574D/",
+    "FrontURL": "https://cerebrodatastorage.blob.core.windows.net/cerebro-cards/official/12005.jpg"
+  }
+]
+]]
