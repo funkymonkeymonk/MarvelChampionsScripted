d66e477a02c65414d02e331ad1dbebbcd033df3c
Will Weaver <monkey@buildingbananas.com>
2022-07-18 16:35:41 -0400

    Fix misspellings, duplicate Spider-Man name collisions, and hp totals for heroes

--- mod/src/MarvelChampionsLCG/Hero_Bag/SP_dr_Hero_Bag.283667.lua
+++ mod/src/MarvelChampionsLCG/Hero_Bag/SP_dr_Hero_Bag.283667.lua
@@ -6,17 +6,20 @@ function getHeroDetails()
   heroDetails.heroDeckId = 356173
   heroDetails.nemesisGuid = "49bccd"
   heroDetails.obligationGuid = "ba1312"
-  heroDetails.hitPoints = 9
+  heroDetails.hitPoints = 14
   heroDetails.counterUrl = "http://cloud-3.steamusercontent.com/ugc/1897721393463027391/971F5CA033F0BD7AFF157E3716D3BD6B745A946C/"
   heroDetails.playmatUrl = "http://cloud-3.steamusercontent.com/ugc/1861691130254392381/73723523D39B54A9710C85B5D8CF6DE445E4E317/"
 
   local extras = {}
 
-  extras["Suit"] =
+  extras[""] =
   {
     guid = "a65e7d",
     offset = {-4.95, 2, 4.64},
     locked = false
   }
+
+  heroDetails["extras"] = extras
+
   return heroDetails
 end
