d213f0eae3d056a09d215eae23a5e7d6700b0501
Shawn Gustafson <shawngus@gmail.com>
2023-08-30 16:38:07 -0600

    Added four newest heroes and twenty modular encounter sets

---
+++ mod/src/MarvelChampionsLCG/Hero_Manager/Domino_Hero_Bag.348324.lua
@@ -0,0 +1,15 @@
+function getHeroDetails()
+  local heroDetails = {}
+
+  heroDetails.identityGuid = "3eb4b7"
+  heroDetails.starterDeckId = 575058
+  heroDetails.heroDeckId = 575060
+  heroDetails.nemesisGuid = "6ee169"
+  heroDetails.obligationGuid = "4fb03b"
+  heroDetails.hitPoints = 9
+  heroDetails.counterUrl = "http://cloud-3.steamusercontent.com/ugc/2062139566431229861/1057FE220EC67587F9129D55A192FC33261D06C8/"
+  heroDetails.playmatUrl = "http://cloud-3.steamusercontent.com/ugc/2035118512673860953/BC9AC409F2BB180F4594115B5A3E499C3B676A2D/"
+
+
+  return heroDetails
+end
